8b07092b95ac6321b2f1b4a01f03f67dce4ea4f5
Essense <movebeyondfear@gmail.com>
2017-10-13 21:55:55 +0800

    Create Stack4Int.java

---
+++ DS/Stack4Int.java
@@ -0,0 +1,32 @@
+package core_java.stack;
+// defines a stack4int 
+public class Stack4Int {
+	private int[] stack;
+	private int top; // point to the top of the stack
+	private int cap;
+	// cap: 栈的容量
+	Stack4Int(int cap){
+		this.cap = cap;
+		stack = new int[cap];
+		top = -1;
+	}
+	void push(int ele){
+		if(top + 1 >= cap)
+			System.out.println("Stack is full...");
+		else
+			stack[++top] = ele;
+	}
+	int pop(){
+		if(top < 0) {
+			System.out.println("Stack is empty...");
+			return 0;
+		}
+		return stack[top--];
+	}
+}
+/*
+**可改进的地方：
+**1 栈的容量可以动态调整的
+**  栈快满时，扩容； 元素个数低于容量的1/4时，容量折半。
+**2 可存储任意类型的元素（使用泛型）
+*/
